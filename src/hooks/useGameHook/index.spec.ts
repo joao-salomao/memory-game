112bbc12d4ea6df5423f544d94202e62efec6800
João Salomão <joaosalomao.613@gmail.com>
2022-04-15 14:09:07 -0300

    feat: create functional hook

--- src/hooks/useGameHook/index.spec.ts
+++ src/hooks/useGameHook/index.spec.ts
@@ -1,9 +1,14 @@
 import { useGameHook } from ".";
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react-hooks";
 
 describe("Use Game Hook", () => {
   it("should start with the board not created", () => {
     const { result } = renderHook(() => useGameHook());
     expect(result.current.boardIsCreated).toBeFalsy();
+  });
+
+  test("the game should not be over if the board is not created", () => {
+    const { result } = renderHook(() => useGameHook());
+    expect(result.current.gameOver).toBeFalsy();
   });
 });
