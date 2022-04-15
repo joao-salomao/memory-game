62a9c4f09d3950e26354b8d2c4624b6ad20d6e34
João Salomão <joaosalomao.613@gmail.com>
2022-04-15 14:08:38 -0300

    refactor: core module

--- src/core/index.spec.ts
+++ src/core/index.spec.ts
@@ -1,70 +1,202 @@
-import { Board, DomainError, Card, markCard, createBoard, gameIsOver } from ".";
+import {
+  createBoard,
+  markCard,
+  gameIsOver,
+  unMarkAllNotFoundAndMarkedCards,
+  hasCardsFromAnotherGroupMarkedAndNotFound,
+  allCardsFromTheGroupAreMarked,
+  setCardsFromGroupAsFounded,
+  Board,
+  InvalidBoardSizeError,
+  CardAlreadyMarkedError,
+} from ".";
 
 describe("Core Module", () => {
-  it("The board size must be greater than zero", () => {
-    const board = () => createBoard(0);
-    expect(board).toThrow(DomainError);
-  });
-
-  it("The amount of cards must be the double of the size", () => {
-    const board1 = createBoard(1);
-    const board2 = createBoard(2);
-    const board3 = createBoard(3);
-
-    expect(board1.length).toBe(2);
-    expect(board2.length).toBe(4);
-    expect(board3.length).toBe(6);
-  });
-
-  it("Should mark card with the same ID", () => {
-    const baseBoard = createBoard(2);
-    const cardToMark = baseBoard[0];
-
-    const newBoard = markCard(cardToMark.id, baseBoard);
-    const markedCard = newBoard.find(({ id }) => id === cardToMark.id);
-
-    expect(markedCard).toBeTruthy();
-    expect(markedCard?.isMarked).toBeTruthy();
-  });
-
-  it('Should set the property "isFound" when all cards of the same group were found', () => {
-    const baseBoard = createBoard(4);
-    const groupId = baseBoard[0].groupId;
-    const cardsToMark = baseBoard.filter((c) => c.groupId === groupId);
-
-    let newBoard: Board = [];
-    cardsToMark.forEach((c) => {
-      newBoard = markCard(c.id, newBoard);
+  describe("Board Creator", () => {
+    it("should throw an error when the board size was less than one", () => {
+      const board = () => createBoard(0);
+      expect(board).toThrow(InvalidBoardSizeError);
     });
 
-    newBoard
-      .filter((c) => c.groupId === groupId)
-      .forEach((c) => {
-        expect(c.isFound).toBeTruthy();
-      });
+    test("the amount of cards on the board should be the double of the provided size", () => {
+      const board1 = createBoard(1);
+      const board2 = createBoard(2);
+      const board3 = createBoard(3);
+
+      expect(board1.length).toBe(2);
+      expect(board2.length).toBe(4);
+      expect(board3.length).toBe(6);
+    });
   });
 
-  it('Should set the property "isMarked" to false to all cards when there is a card marked and a card from another group is marked', () => {
-    const baseBoard = createBoard(4);
-    const card1 = baseBoard[0];
-    const card2 = baseBoard.find((c) => c.groupId !== card1.groupId) as Card;
+  describe("Card Marker", () => {
+    it('should set the property "isMarked" of the card to true', () => {
+      const board: Board = [
+        { id: 1, groupId: 1, isFound: false, isMarked: false },
+        { id: 2, groupId: 1, isFound: false, isMarked: false },
+        { id: 3, groupId: 2, isFound: false, isMarked: false },
+        { id: 4, groupId: 2, isFound: false, isMarked: false },
+      ];
 
-    const updateBoard1 = markCard(card1.id, baseBoard);
-    const updateBoard2 = markCard(card2.id, updateBoard1);
-    const allCardsAreNotMarked = updateBoard2.every(
-      (c) => c.isMarked === false
-    );
+      const result: Board = markCard(3, board);
 
-    expect(allCardsAreNotMarked).toBeTruthy();
+      const expectedBoard: Board = [
+        { id: 1, groupId: 1, isFound: false, isMarked: false },
+        { id: 2, groupId: 1, isFound: false, isMarked: false },
+        { id: 3, groupId: 2, isFound: false, isMarked: true },
+        { id: 4, groupId: 2, isFound: false, isMarked: false },
+      ];
+
+      expect(result).toEqual(expectedBoard);
+    });
+
+    it("should throw an error if the card is already marked", () => {
+      const board: Board = [
+        { id: 1, groupId: 1, isFound: false, isMarked: true },
+        { id: 2, groupId: 1, isFound: false, isMarked: false },
+        { id: 3, groupId: 2, isFound: false, isMarked: false },
+        { id: 4, groupId: 2, isFound: false, isMarked: false },
+      ];
+
+      expect(() => markCard(1, board)).toThrow(CardAlreadyMarkedError);
+    });
   });
 
-  it("Should return true when game is over", () => {
-    const board = createBoard(10);
+  describe("Game Over Checker", () => {
+    it('should return "true" when the game is over', () => {
+      const board: Board = [
+        { id: 1, groupId: 1, isFound: true, isMarked: true },
+        { id: 2, groupId: 1, isFound: true, isMarked: true },
+        { id: 3, groupId: 2, isFound: true, isMarked: true },
+        { id: 4, groupId: 2, isFound: true, isMarked: true },
+      ];
 
-    const newBoard = board
-      .sort((c) => c.groupId)
-      .reduce((currentBoard, card) => markCard(card.id, currentBoard), board);
+      const result = gameIsOver(board);
 
-    expect(gameIsOver(newBoard)).toBeTruthy();
+      expect(result).toBeTruthy();
+    });
+
+    it('should return "false" when the game is not over', () => {
+      const board: Board = [
+        { id: 1, groupId: 1, isFound: false, isMarked: false },
+        { id: 2, groupId: 1, isFound: false, isMarked: false },
+        { id: 3, groupId: 2, isFound: true, isMarked: true },
+        { id: 4, groupId: 2, isFound: true, isMarked: true },
+      ];
+
+      const result = gameIsOver(board);
+
+      expect(result).toBeFalsy();
+    });
+  });
+
+  describe("Has cards from another group marked and not found checker", () => {
+    it('should return "false" when there is not cards from another group marked and not found checker', () => {
+      const board: Board = [
+        { id: 1, groupId: 1, isFound: false, isMarked: true },
+        { id: 2, groupId: 1, isFound: false, isMarked: false },
+        { id: 3, groupId: 2, isFound: false, isMarked: false },
+        { id: 4, groupId: 2, isFound: false, isMarked: false },
+      ];
+
+      const result: boolean = hasCardsFromAnotherGroupMarkedAndNotFound(
+        1,
+        board
+      );
+
+      expect(result).toBeFalsy();
+    });
+
+    it('should return "true" when there is cards from another group marked and not found checker', () => {
+      const board: Board = [
+        { id: 1, groupId: 1, isFound: false, isMarked: true },
+        { id: 2, groupId: 1, isFound: false, isMarked: false },
+        { id: 3, groupId: 2, isFound: false, isMarked: true },
+        { id: 4, groupId: 2, isFound: false, isMarked: false },
+      ];
+
+      const result: boolean = hasCardsFromAnotherGroupMarkedAndNotFound(
+        1,
+        board
+      );
+
+      expect(result).toBeTruthy();
+    });
+  });
+
+  describe("Un mark All Not Found And Marked Cards", () => {
+    it("should un mark all not found and marked cards", () => {
+      const board: Board = [
+        { id: 1, groupId: 1, isFound: false, isMarked: true },
+        { id: 2, groupId: 1, isFound: false, isMarked: false },
+        { id: 3, groupId: 2, isFound: false, isMarked: true },
+        { id: 4, groupId: 2, isFound: false, isMarked: false },
+        { id: 5, groupId: 3, isFound: true, isMarked: true },
+        { id: 6, groupId: 3, isFound: true, isMarked: true },
+      ];
+
+      const result: Board = unMarkAllNotFoundAndMarkedCards(board);
+
+      const expectedBoard: Board = [
+        { id: 1, groupId: 1, isFound: false, isMarked: false },
+        { id: 2, groupId: 1, isFound: false, isMarked: false },
+        { id: 3, groupId: 2, isFound: false, isMarked: false },
+        { id: 4, groupId: 2, isFound: false, isMarked: false },
+        { id: 5, groupId: 3, isFound: true, isMarked: true },
+        { id: 6, groupId: 3, isFound: true, isMarked: true },
+      ];
+
+      expect(result).toEqual(expectedBoard);
+    });
+  });
+
+  describe("All Cards From The Group Are Marked Checker", () => {
+    it('should return "true" when all cards from the group are marked', () => {
+      const board: Board = [
+        { id: 1, groupId: 1, isFound: false, isMarked: true },
+        { id: 2, groupId: 1, isFound: false, isMarked: true },
+        { id: 3, groupId: 2, isFound: false, isMarked: false },
+        { id: 4, groupId: 2, isFound: false, isMarked: false },
+      ];
+
+      const result: boolean = allCardsFromTheGroupAreMarked(1, board);
+
+      expect(result).toBeTruthy();
+    });
+
+    it('should return "false" when all cards from the group are not marked', () => {
+      const board: Board = [
+        { id: 1, groupId: 1, isFound: false, isMarked: false },
+        { id: 2, groupId: 1, isFound: false, isMarked: true },
+        { id: 3, groupId: 2, isFound: false, isMarked: false },
+        { id: 4, groupId: 2, isFound: false, isMarked: false },
+      ];
+
+      const result: boolean = allCardsFromTheGroupAreMarked(1, board);
+
+      expect(result).toBeFalsy();
+    });
+  });
+
+  describe("Set Cards From Group As Founded Action", () => {
+    it("should set all cards from the group as founded", () => {
+      const board: Board = [
+        { id: 1, groupId: 1, isFound: false, isMarked: true },
+        { id: 2, groupId: 1, isFound: false, isMarked: true },
+        { id: 3, groupId: 2, isFound: false, isMarked: false },
+        { id: 4, groupId: 2, isFound: false, isMarked: false },
+      ];
+
+      const result: Board = setCardsFromGroupAsFounded(1, board);
+
+      const expectedBoard: Board = [
+        { id: 1, groupId: 1, isFound: true, isMarked: true },
+        { id: 2, groupId: 1, isFound: true, isMarked: true },
+        { id: 3, groupId: 2, isFound: false, isMarked: false },
+        { id: 4, groupId: 2, isFound: false, isMarked: false },
+      ];
+
+      expect(result).toEqual(expectedBoard);
+    });
   });
 });
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,4 @@
+// Game types
 export type Card = {
   id: number;
   groupId: number;
@@ -5,52 +6,23 @@ export type Card = {
   isMarked: boolean;
 };
 export type Board = Array<Card>;
-export type BoardMaker = (size: number) => Board;
-export type CardsMaker = (amount: number) => Board;
+
+// Handlers
+export type BoardCreator = (size: number) => Board;
+export type CardsCreator = (amount: number) => Board;
 export type CardMarker = (cardId: Card["id"], board: Board) => Board;
 export type CardsAmountCalculator = (size: number) => number;
 export type GameOverChecker = (board: Board) => boolean;
+
+// Errors
 export class DomainError extends Error {}
+export class CardAlreadyMarkedError extends DomainError {}
+export class InvalidBoardSizeError extends DomainError {}
 
 export const gameIsOver: GameOverChecker = (board: Board): boolean =>
   board.every((c) => c.isFound);
 
-export const markCard: CardMarker = (
-  cardId: Card["id"],
-  board: Board
-): Board => {
-  const cardToMark = board.find((c) => c.id === cardId);
-
-  const hasOtherCardFromOtherGroupMarked = board.find(
-    (c) => c.isMarked && !c.isFound && c.groupId !== cardToMark?.groupId
-  );
-
-  if (hasOtherCardFromOtherGroupMarked) {
-    return board.map((c) => ({ ...c, isMarked: c.isFound || false }));
-  }
-
-  let newBoard = board.map((card) => {
-    const newCard = { ...card };
-    if (newCard.id === cardId) newCard.isMarked = true;
-    return newCard;
-  });
-
-  const allGroupMembersAreMarked = newBoard
-    .filter((c) => c.groupId === cardToMark?.groupId)
-    .every((c) => c.isMarked);
-
-  if (allGroupMembersAreMarked) {
-    newBoard = newBoard.map((card) => {
-      const newCard = { ...card };
-      if (newCard.groupId === cardToMark?.groupId) newCard.isFound = true;
-      return newCard;
-    });
-  }
-
-  return newBoard;
-};
-
-export const createBoard: BoardMaker = (size: number): Board => {
+export const createBoard: BoardCreator = (size: number): Board => {
   const amount = getCardsAmountToGenerate(size);
   return createCards(amount);
 };
@@ -59,12 +31,14 @@ const getCardsAmountToGenerate: CardsAmountCalculator = (
   size: number
 ): number => {
   if (size < 1)
-    throw new DomainError("The board size must be greater than zero.");
+    throw new InvalidBoardSizeError(
+      "The board size must be greater than zero."
+    );
 
   return size * 2;
 };
 
-const createCards: CardsMaker = (amount: number): Board => {
+const createCards: CardsCreator = (amount: number): Board => {
   const cards = [];
 
   for (let i = 0; i < amount; i = i + 2) {
@@ -84,4 +58,75 @@ const createCards: CardsMaker = (amount: number): Board => {
   }
 
   return cards;
+};
+
+export const markCard: CardMarker = (
+  cardId: Card["id"],
+  board: Board
+): Board => {
+  if (cardIsAlreadyMarkedChecker(cardId, board)) {
+    throw new CardAlreadyMarkedError("The card is already marked");
+  }
+
+  return board.map((card) => {
+    const newCard = { ...card };
+
+    if (card.id === cardId) {
+      newCard.isMarked = true;
+    }
+
+    return newCard;
+  });
+};
+
+const cardIsAlreadyMarkedChecker = (
+  cardId: Card["id"],
+  board: Board
+): boolean => {
+  return board.some((card) => card.id === cardId && card.isMarked);
+};
+
+export const hasCardsFromAnotherGroupMarkedAndNotFound = (
+  groupId: Card["groupId"],
+  board: Board
+): boolean => {
+  return board.some(
+    (card) => card.groupId !== groupId && card.isMarked && !card.isFound
+  );
+};
+
+export const unMarkAllNotFoundAndMarkedCards = (board: Board): Board => {
+  return board.map((card) => {
+    const newCard = { ...card };
+
+    if (newCard.isMarked && !card.isFound) {
+      newCard.isMarked = false;
+    }
+
+    return newCard;
+  });
+};
+
+export const allCardsFromTheGroupAreMarked = (
+  groupId: Card["groupId"],
+  board: Board
+): boolean => {
+  return board
+    .filter((card) => card.groupId === groupId)
+    .every((card) => card.isMarked);
+};
+
+export const setCardsFromGroupAsFounded = (
+  groupId: Card["groupId"],
+  board: Board
+): Board => {
+  return board.map((card) => {
+    const newCard = { ...card };
+
+    if (card.groupId === groupId) {
+      newCard.isFound = true;
+    }
+
+    return newCard;
+  });
 };
