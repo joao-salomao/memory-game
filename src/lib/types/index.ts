d2b5fe653c79eb9189661820c53aa3859844258f
João Salomão <joaosalomao.613@gmail.com>
2022-05-29 17:06:05 -0300

    fix: types

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,31 +1,24 @@
 // Data structures
-export type Value = any;
 export type Card = {
   id: number;
   groupId: number;
   isFound: boolean;
   isMarked: boolean;
-  value: Value;
+  value: CardInfo;
 };
+export type CardInfo = {
+  title?: string;
+  img?: string;
+  color?: string;
+}; 
 export type Board = Card[];
-export interface EnumeratedColor {
-  number: number;
-  color: string;
-}
-export interface Image {
-  title: string;
-  img: string;
-}
-
-export type CardWithEnumeratedColor = Card & { value: EnumeratedColor };
-export type CardWithImage = Card & { value: Image };
 export type Stage = {
   name: string;
-  values: EnumeratedColor[] | Image[];
+  values: CardInfo[];
 };
 
 // Functions
-export type BoardCreator = (values: Array<Value>) => Board;
+export type BoardCreator = (values: Array<CardInfo>) => Board;
 export type CardsCreator = (amountOfGroups: number) => Board;
 export type CardMarker = (cardToMark: Card, board: Board) => Board;
 export type AmountOfCardsGroupCalculator = (size: number) => number;
