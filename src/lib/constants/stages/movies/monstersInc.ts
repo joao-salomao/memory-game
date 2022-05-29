277f4123976677f35f02087d71d95245037bc0be
João Salomão <joaosalomao.613@gmail.com>
2022-05-29 17:20:11 -0300

    feat: add new characters

--- src/lib/constants/stages/movies/monstersInc.ts
+++ src/lib/constants/stages/movies/monstersInc.ts
@@ -1,7 +1,9 @@
 import { Stage } from "lib/types";
 import Boo from "assets/monsters-inc/Boo.png";
 import Sullivan from "assets/monsters-inc/Sullivan.png";
-import MikeWazowski from "assets/monsters-inc/MikeWazowski.png";
+import MikeWazowski from "assets/monsters-inc/Mike_Wazowski.png";
+import RandallBoggs from "assets/monsters-inc/Randall_Boggs.png";
+import Celia_Mae from 'assets/monsters-inc/Celia_Mae.png'
 
 export const MonstersInc: Stage = {
   name: "Monsters, Inc.",
@@ -13,6 +15,12 @@ export const MonstersInc: Stage = {
 
     {
       img: Sullivan,
+    },
+    {
+      img: RandallBoggs,
+    },
+    {
+      img: Celia_Mae,
     },
   ],
 };
